1b88216180eadc34f69c180b0afe7ab1c5b34ca3
mohadesegharakhanlo <47597127+mohadesegharakhanlo@users.noreply.github.com>
2019-08-04 21:45:28 +0430

    new not from js and event in js

--- one.js
+++ one.js
@@ -5,3 +5,15 @@ function f1(){
   }
 }
 //f1()
+function focusfunc(){
+  alert("I am here!")
+}
+function onplurfunc(){
+  alert("I out here")
+}
+function myfunc(){
+  document.getElementById('myspan').innerHTML=2;
+}
+function firstnamefunc(){
+  document.getElementById("firstnames").innerHTML=eval(document.getElementById("firstnames").innerHTML)+1
+}
