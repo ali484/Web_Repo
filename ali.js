ab8be42708687fda6819c36e4e3d2a3abee2efd5
Ali_Pileforooshha <alipileforooshha@gmail.com>
2019-08-04 13:55:10 +0430

    sunday noon

--- ali.js
+++ ali.js
@@ -33,6 +33,25 @@ document.getElementById('obj_id').innerHTML = person.fullname();
 function ShowDate(){
     document.getElementById('func_time').innerHTML=Date();
 }
-var carName = 'prideshiiiit';
-var length = carName.length; //shows the length of the string
-
+var string = 'this is just a test string to use the string functions';
+var length = string.length; //shows the length of the string
+var position_first = string.indexOf('string');//shows the starting position of the 
+document.getElementById('string_test_1').innerHTML = position_first;
+var position_last = string.lastIndexOf('string');
+document.getElementById('string_test_2').innerHTML = position_last;
+var search_pos = string.search('to use the');
+document.getElementById('string_test_3').innerHTML = search_pos;
+var string_sliced = string.slice(8,12);
+document.getElementById('string_test_4').innerHTML = string_sliced;
+var string2 = "Kiwi , banana , Pride , ME , javascript";
+var replaced = string2.replace('banana' , 'milkchoclate');
+document.getElementById('string_test_5').innerHTML = replaced;
+var toupper = string2.toUpperCase();
+document.getElementById('string_test_6').innerHTML = toupper;
+var tolowwer = string2.toLowerCase();
+document.getElementById('string_test_7').innerHTML = tolowwer;
+// var trimed = string2.trim(); it removes spaces from both  start and end of the
+// string.charAt(20) returns the character at that position
+// string[0]; returns the first character of the string
+var arr = string2.split(",");//splits the string into array by the commas
+document.getElementById('string_test_8').innerHTML = arr[2];
