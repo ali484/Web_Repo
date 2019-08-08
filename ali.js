cfd85994543eec7987fe6dd5765905c237d4823c
Ali_Pileforooshha <alipileforooshha@gmail.com>
2019-08-08 18:11:17 +0430

    pushing! =)))

--- ali.js
+++ ali.js
@@ -53,5 +53,32 @@ document.getElementById('string_test_7').innerHTML = tolowwer;
 // var trimed = string2.trim(); it removes spaces from both  start and end of the
 // string.charAt(20) returns the character at that position
 // string[0]; returns the first character of the string
-var arr = string2.split(",");//splits the string into array by the commas
-document.getElementById('string_test_8').innerHTML = arr[2];
+var str_arr = string2.split(",");//splits the string into array by the commas
+document.getElementById('string_test_8').innerHTML = str_arr[2];
+console.log(string2);
+// var console_test = prompt("enter a number"); used for getting a number from user
+//var x = 123e5;     12300000
+//var y = 123e-5;    0.00123=
+//var x = "100"; var = "10";
+// while(x!=Infinity){
+//     x = x*x;
+// }
+//var x=0xFF;
+//var number; number.toString(2); returns the number to string to base 2
+var math_number = 9.4291;
+console.log("number 9.4291 with exponential 3 is " + math_number.toExponential(3)); // returns a string
+console.log("number 9.4291 with fixed 3 is " + math_number.toFixed(3)); // returns a string
+console.log("number 9.4291 with fixed 3 is " + math_number.toPrecision(3)); // returns a string
+console.log("the function number() can turn its content string or whatever into number '  10.33    'is"+Number('  10.33    ') );
+console.log("Number(true) is "+Number(true) );
+console.log("Number('john') is "+Number("john") );
+console.log("Number(new Date('2018-08-08')) is "+Number(new Date("2018-08-08")) );
+console.log("parseInt('10 20 30') is "+parseInt('10 20 30'));
+console.log("parseInt('10.33') is "+parseInt('10.33'));
+console.log("parseInt('10 years') is "+parseInt('10 years'));
+console.log("parseInt('years 10') is "+parseInt('years 10'));
+console.log("parseFloat('10 20 30') is "+parseFloat('10 20 30'));
+console.log("parseFloat('10.33') is "+parseFloat('10.33'));
+console.log("parseFloat('10 years') is "+parseFloat('10 years'));
+console.log("parseFloat('years 10') is "+parseFloat('years 10'));
+var fruitArr = ['kiwi' , 'grape' , 'mango' , 'pineapple' , 'watermelon'];
