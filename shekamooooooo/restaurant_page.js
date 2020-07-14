36886a1a18a938e28c3ba594baf751b1bfa8009c
ali484 <micahelwwww@gmail.com>
2020-07-14 05:49:15 +0430

    sss

--- shekamooooooo/restaurant_page.js
+++ shekamooooooo/restaurant_page.js
@@ -24,3 +24,19 @@ function reduceorder()
     this.parentNode.setAttribute('data-count' , `${count}`);
     this.parentNode.childNodes[3].innerHTML = count;
 }
+
+
+var joojeh = document.querySelector('[data-nav = jooje]');
+var khoresht = document.querySelector('[data-nav = khoresht]');
+var drinks = document.querySelector('[data-nav = drinks]');
+var sides = document.querySelector('[data-nav = sides]');
+var comments = document.querySelector('[data-nav = comments]');
+
+var restaurantNav = document.querySelector('.restaurant-nav');
+restaurantNav.childNodes.forEach(item => item.addEventListener('mouseover' , changepage));
+
+function changepage(){
+    console.log(this);
+    console.log('sssss');
+    document.querySelectorAll('.section').forEach(item => (item.dataset.nav === this.dataset.nav) ? item.style.display = 'block' : item.style.display = 'none');
+}
